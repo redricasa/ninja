371c00589f074ab327ffc102a166c19e3aff7f34
redricasa <fredeb26@gmail.com>
2021-08-30 22:39:09 -0700

    class constructor

---
+++ index.js
@@ -0,0 +1,11 @@
+class Ninja {
+    constructor(name , health, speed = 3) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+
+
+    }
+}
+
+const ninjaOne = new Ninja("Snake Eyes");
